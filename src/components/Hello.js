5d69896d1c2c808a9bb1629ed027c82bec53e61b
Martin Sone <kwangmart12@gmail.com>
2020-10-25 17:58:07 +0100

    hot module fn added

---
+++ src/components/Hello.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Hello extends Component {
+    render() {
+        return (
+            <div>
+               <h2>Hello App!</h2> 
+            </div>
+        );
+    }
+}
+
+export default Hello;
