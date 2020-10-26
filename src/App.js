71372f804b5e2cd2be62086f891d4e99c66c46d5
Martin Sone <kwangmart12@gmail.com>
2020-10-26 13:45:11 +0100

    static table created in home.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import 'antd/dist/antd.css';
 import './App.css';
+import HomeComponent from './component/TableInfo/home';
 
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="home">
-      
-    </div>
-  );
+  render() {
+
+    return (
+      <div className="App">
+        <Switch>
+          <Route path="/" exact title="Home" component={HomeComponent} />
+        </Switch>
+      </div>
+
+    );
+  }
 }
 
 export default App;
