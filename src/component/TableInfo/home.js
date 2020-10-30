f888ac27ff7de52b7ac10c1bdc19090bc6e6057a
Martin Sone <kwangmart12@gmail.com>
2020-10-30 13:38:02 +0100

    mock_data file added

--- src/component/TableInfo/home.js
+++ src/component/TableInfo/home.js
@@ -246,7 +246,7 @@ class HomeComponent extends Component {
   }
 
   // email modal
-  showEmailModal = () => {
+  showEmailModal = () => { 
     this.setState({ emailModelVisibility: true });
   };
 
